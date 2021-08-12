38370b4ca1f406b319d3e23f196f19e48ca33eda
Sayali Vilas Phatak <79622561+sayaliphatak9628@users.noreply.github.com>
2021-08-12 18:02:00 +0530

    vue basics

---
+++ modal-project/src/App.vue
@@ -0,0 +1,96 @@
+<template>
+    <h1>{{ title }}</h1>
+
+    <!-- template refs -->
+    <!-- <input type="text" ref="name" />
+    <button @click="handleClick">click me</button> -->
+
+    <!-- passing data with props -->
+    <!-- <Modal
+        header="Sign up for the Giveaway!"
+        text="Grab your ninja swag for half price!"
+    /> -->
+
+    <!-- data binding -->
+    <!-- <Modal :header="header" :text="text" theme="sale" /> -->
+
+    <!-- custom events -->
+    <teleport to=".modals" v-if="showModal">
+        <!--teleport to index.html page, replace div with 'teleport to=".modals"' -->
+        <!-- <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/> -->
+        <Modal theme="sale" @close="toggleModal">
+            <template v-slot:links>
+                <button><a href="#">sign up </a></button> |
+                <button><a href="#">more info</a></button>
+            </template>
+            <h1>Ninja Giveaway!</h1>
+            <p>Grab your ninja swag for half price!</p>
+        </Modal>
+    </teleport>
+    <p>Welcome...</p>
+    <button @click="toggleModal">open modal</button>
+
+    <!-- challenge -->
+    <div v-if="showModalTwo">
+        <!-- <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/> -->
+        <Modal @close="toggleModal2">
+            <h1>Sign up for the newsletter!</h1>
+            <p>For updates,promocodes</p>
+        </Modal>
+    </div>
+    <button @click="toggleModal2">open modal two</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue';
+
+export default {
+    name: 'App',
+    components: {
+        Modal,
+    },
+
+    data() {
+        return {
+            title: 'My first vue app :)',
+            header: 'Sign up for the Giveaway!',
+            text: 'Grab your ninja swag for half price!',
+            showModal: false,
+            showModalTwo: false,
+        };
+    },
+
+    methods: {
+        handleClick() {
+            console.log(this.$refs.name);
+            this.$refs.name.classList.add('active');
+            this.$refs.name.focus();
+        },
+
+        toggleModal() {
+            this.showModal = !this.showModal;
+        },
+        toggleModal2() {
+            this.showModalTwo = !this.showModalTwo;
+        },
+    },
+};
+</script>
+
+<style>
+#app,
+.modals {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+h1 {
+    border-bottom: 1px solid #ddd;
+    display: inline-block;
+    padding-bottom: 10px;
+}
+</style>
---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+
+    <div>
+        <h1>Ninja Reaction Timer</h1>
+        <button @click="start" :disabled="isPlaying">Play</button>
+        <Block v-if="isPlaying" v-bind:delay="delay" @end="endGame" />
+        <!-- <p v-if="showResults">Reaction Time: {{ score }} ms</p> -->
+        <Result v-if="showResults" :score="score" />
+    </div>
+</template>
+
+<script>
+import Block from './components/Block.vue';
+import Result from './components/Result.vue';
+
+export default {
+    name: 'App',
+    components: {
+        Block,
+        Result,
+    },
+    data() {
+        return {
+            isPlaying: false,
+            delay: null,
+            score: null,
+            showResults: false,
+        };
+    },
+    methods: {
+        start() {
+            this.delay = 2000 + Math.random() * 5000;
+            this.isPlaying = true;
+            //    console.log(this.delay);
+            this.showResults = false;
+        },
+        endGame(reactionTime) {
+            this.score = reactionTime;
+            this.isPlaying = false;
+            this.showResults = true;
+        },
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    /* font-family: Bradley Hand, cursive; */
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
+}
+
+button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+}
+
+button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+}
+</style>
