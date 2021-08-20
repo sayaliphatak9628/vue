ec1892efb4e05c5612440d45ad10457ee48ebb1b
Sayali Vilas Phatak <79622561+sayaliphatak9628@users.noreply.github.com>
2021-08-20 18:19:44 +0530

    vue project

---
+++ covid-tracker/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+      <router-view />
+    </div>
+</template>
+
+<script>
+import Header from '@components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
